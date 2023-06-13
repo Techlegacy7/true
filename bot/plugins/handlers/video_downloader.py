4c9cdb3418dc8c462f7b36d148e8e9c236b73826
kevinnadar <jesikamaraj@gmail.com>
2023-06-13 11:40:26 +0530

    .

--- bot/plugins/handlers/video_downloader.py
+++ bot/plugins/handlers/video_downloader.py
@@ -116,6 +116,8 @@ async def echo(bot, update):
             disable_web_page_preview=True,
         )
         return False
+    # logger.info(response_json)
+    inline_keyboard = []
     if t_response:
         # logger.info(t_response)
         x_reponse = t_response
@@ -123,15 +125,11 @@ async def echo(bot, update):
             x_reponse, _ = x_reponse.split("\n")
         response_json = json.loads(x_reponse)
         save_ytdl_json_path = (
-            Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".json"
+            f"{Config.DOWNLOAD_LOCATION}/{str(update.from_user.id)}.json"
         )
         with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
             json.dump(response_json, outfile, ensure_ascii=False)
-        # logger.info(response_json)
-        inline_keyboard = []
-        duration = None
-        if "duration" in response_json:
-            duration = response_json["duration"]
+        duration = response_json["duration"] if "duration" in response_json else None
         if "formats" in response_json:
             for formats in response_json["formats"]:
                 format_id = formats.get("format_id")
@@ -142,16 +140,19 @@ async def echo(bot, update):
                 approx_file_size = ""
                 if "filesize" in formats:
                     approx_file_size = humanbytes(formats["filesize"])
-                cb_string_video = "{}|{}|{}".format("video", format_id, format_ext)
-                cb_string_file = "{}|{}|{}".format("file", format_id, format_ext)
-                if format_string is not None and not "audio only" in format_string:
+                cb_string_video = f"video|{format_id}|{format_ext}"
+                cb_string_file = f"file|{format_id}|{format_ext}"
+                if (
+                    format_string is not None
+                    and "audio only" not in format_string
+                ):
                     ikeyboard = [
                         InlineKeyboardButton(
-                            "S " + format_string + " video " + approx_file_size + " ",
+                            f"S {format_string} video {approx_file_size} ",
                             callback_data=(cb_string_video).encode("UTF-8"),
                         ),
                         InlineKeyboardButton(
-                            "D " + format_ext + " " + approx_file_size + " ",
+                            f"D {format_ext} {approx_file_size} ",
                             callback_data=(cb_string_file).encode("UTF-8"),
                         ),
                     ]
@@ -169,34 +170,34 @@ async def echo(bot, update):
                     ]
                 inline_keyboard.append(ikeyboard)
             if duration is not None:
-                cb_string_64 = "{}|{}|{}".format("audio", "64k", "mp3")
-                cb_string_128 = "{}|{}|{}".format("audio", "128k", "mp3")
-                cb_string = "{}|{}|{}".format("audio", "320k", "mp3")
-                inline_keyboard.append(
-                    [
-                        InlineKeyboardButton(
-                            "MP3 " + "(" + "64 kbps" + ")",
-                            callback_data=cb_string_64.encode("UTF-8"),
-                        ),
-                        InlineKeyboardButton(
-                            "MP3 " + "(" + "128 kbps" + ")",
-                            callback_data=cb_string_128.encode("UTF-8"),
-                        ),
-                    ]
-                )
-                inline_keyboard.append(
-                    [
-                        InlineKeyboardButton(
-                            "MP3 " + "(" + "320 kbps" + ")",
-                            callback_data=cb_string.encode("UTF-8"),
-                        )
-                    ]
+                cb_string_64 = "audio|64k|mp3"
+                cb_string_128 = "audio|128k|mp3"
+                cb_string = "audio|320k|mp3"
+                inline_keyboard.extend(
+                    (
+                        [
+                            InlineKeyboardButton(
+                                "MP3 " + "(" + "64 kbps" + ")",
+                                callback_data=cb_string_64.encode("UTF-8"),
+                            ),
+                            InlineKeyboardButton(
+                                "MP3 " + "(" + "128 kbps" + ")",
+                                callback_data=cb_string_128.encode("UTF-8"),
+                            ),
+                        ],
+                        [
+                            InlineKeyboardButton(
+                                "MP3 " + "(" + "320 kbps" + ")",
+                                callback_data=cb_string.encode("UTF-8"),
+                            )
+                        ],
+                    )
                 )
         else:
             format_id = response_json["format_id"]
             format_ext = response_json["ext"]
-            cb_string_file = "{}|{}|{}".format("file", format_id, format_ext)
-            cb_string_video = "{}|{}|{}".format("video", format_id, format_ext)
+            cb_string_file = f"file|{format_id}|{format_ext}"
+            cb_string_video = f"video|{format_id}|{format_ext}"
             inline_keyboard.append(
                 [
                     InlineKeyboardButton(
@@ -207,8 +208,8 @@ async def echo(bot, update):
                     ),
                 ]
             )
-            cb_string_file = "{}={}={}".format("file", format_id, format_ext)
-            cb_string_video = "{}={}={}".format("video", format_id, format_ext)
+            cb_string_file = f'file={format_id}={format_ext}'
+            cb_string_video = f'video={format_id}={format_ext}'
             inline_keyboard.append(
                 [
                     InlineKeyboardButton(
@@ -228,9 +229,9 @@ async def echo(bot, update):
             thumbnail_image = response_json["thumbnail"]
         thumb_image_path = DownLoadFile(
             thumbnail_image,
-            Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".webp",
+            f"{Config.DOWNLOAD_LOCATION}/{str(update.from_user.id)}.webp",
             Config.CHUNK_SIZE,
-            None,  # bot,
+            None,
             Script.DOWNLOAD_START,
             update.id,
             update.chat.id,
@@ -250,10 +251,8 @@ async def echo(bot, update):
             reply_to_message_id=update.id,
         )
     else:
-        # fallback for nonnumeric port a.k.a seedbox.io
-        inline_keyboard = []
-        cb_string_file = "{}={}={}".format("file", "LFO", "NONE")
-        cb_string_video = "{}={}={}".format("video", "OFL", "ENON")
+        cb_string_file = 'file=LFO=NONE'
+        cb_string_video = 'video=OFL=ENON'
         inline_keyboard.append(
             [
                 InlineKeyboardButton(
