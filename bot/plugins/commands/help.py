58b7c60e632bb55fecb3b59641484f551ec7fb78
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-07-11 11:24:24 +0530

    Update help.py

--- bot/plugins/commands/help.py
+++ bot/plugins/commands/help.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from bot.config import Script, Buttons
 
-
 @Client.on_message(filters.command("help") & filters.private & filters.incoming)
 @Client.on_message(
     filters.regex(Buttons.help_regex) & filters.private & filters.incoming
