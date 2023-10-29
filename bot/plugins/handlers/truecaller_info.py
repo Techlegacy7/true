0cd1b8c8ab41c11d70ba4ad92578fb54d3a3a36a
cardboard-face <149292864+cardboard-face@users.noreply.github.com>
2023-10-29 07:45:59 +0000

    Update truecaller_info.py

--- bot/plugins/handlers/truecaller_info.py
+++ bot/plugins/handlers/truecaller_info.py
@@ -1,23 +1,23 @@
 import re
 import traceback
-from pyrogram import Client, filters
+from pyrogram import Client, filters, types
 from pyrogram.types import Message
 from bot.config import Buttons
 from bot.utils import search_number
 import html2text
-
+import json
 @Client.on_message(
     filters.regex(f"{Buttons.trucaller_info_text}") & filters.private & filters.incoming
 )
 async def truecaller_info(client: Client, message: Message):
     ask = await message.chat.ask(
         text="Send me the number you want to search for.\n\n"
-        "Example: `+919876543210`\n"
+        "Example: +919876543210\n"
         "Only Indian numbers are supported.\n\n",
         filters=filters.text,
         timeout=3600,
     )
-
+    print(ask)
     regex = r"^\+?[1-9]\d{1,14}$"
 
     if not re.search(regex, ask.text):
@@ -27,26 +27,22 @@ async def truecaller_info(client: Client, message: Message):
     txt = await message.reply_text("Searching for the number...")
 
     try:
-        result = await search_number(ask.text)
+        result = await search_number(ask.text
     except Exception as e:
-        await message.reply_text(f"Error: `{e}`")
+        await message.reply_text(f"Error : `{e}`")
         return
-
     try:
-        data = result["data"][0]
         text = f"""Information found on Truecaller for {ask.text}:
-
-Name: {data.get('name')}
-Gender: {data.get('gender')}
-Score: {data.get('score')}
-Carrier: {data.get('phones', [{}])[0].get('carrier') if data.get('phones') else None}
-Address: {data.get('addresses', [{}])[0].get('city') if data.get('addresses') else None} 
-Email: {data.get('internetAddresses', [{}])[0].get('id') if data.get('internetAddresses') else None}
+Name: {result["data"]["data"][0]["name"]}
+Score: {result["data"]["data"][0]["score"]}
+Carrier: {result["data"]["data"][0]["phones"][0]["carrier"] if result["data"]["data"][0]["phones"][0] else None}
+Address: {result["data"]["data"][0]["addresses"][0]["city"] if result["data"]["data"][0]["addresses"][0] else None} 
+Email: {result["data"]["data"][0]["internetAddresses"][0]["id"] if result["data"]["data"][0]["internetAddresses"][0] else None}
 """
         await txt.edit(text=text, disable_web_page_preview=True)
     except Exception as e:
         traceback.print_exc()
-        await txt.edit(f"Error: `{e}`")
+        await txt.edit(f"Error: {e}")
 
 
 def html_to_markdown(html_content):
