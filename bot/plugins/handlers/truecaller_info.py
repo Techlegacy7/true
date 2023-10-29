e7db5a2534caf3b7c10e43da54f3594c011ffe6c
cardboard-face <149292864+cardboard-face@users.noreply.github.com>
2023-10-29 15:22:54 +0530

    Update truecaller_info.py

--- bot/plugins/handlers/truecaller_info.py
+++ bot/plugins/handlers/truecaller_info.py
@@ -38,9 +38,9 @@ async def truecaller_info(client: Client, message: Message):
         
 Name: {result["data"]["data"][0]["name"]}
 Score: {score_Percent}%
-Carrier: {result["data"]["data"][0]["phones"][0]["carrier"] if result["data"]["data"][0]["phones"][0] else None}
-Address: {result["data"]["data"][0]["addresses"][0]["city"] if result["data"]["data"][0]["addresses"][0] else None} 
-Email: {result["data"]["data"][0]["internetAddresses"][0]["id"] if result["data"]["data"][0]["internetAddresses"][0] else None}
+Carrier: {result["data"]["data"][0]["phones"][0]["carrier"] if result["data"]["data"][0]["phones"] else None}
+Address: {result["data"]["data"][0]["addresses"][0]["city"] if result["data"]["data"][0]["addresses"] else None} 
+Email: {result["data"]["data"][0]["internetAddresses"][0]["id"] if result["data"]["data"][0]["internetAddresses"] else None}
 """
         await txt.edit(text=text, disable_web_page_preview=True)
     except Exception as e:
