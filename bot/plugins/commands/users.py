34f81fd03ecc62c0c452a3394ef6abaa0bb8817b
cardboard-face <149292864+cardboard-face@users.noreply.github.com>
2023-11-30 20:58:38 +0530

    Update users.py

--- bot/plugins/commands/users.py
+++ bot/plugins/commands/users.py
@@ -6,8 +6,8 @@ from bot.config import Config
     filters.command("users") & filters.user(Config.ADMINS) & filters.incoming
 )
 async def users_count(bot, message: types.Message):
-    total_users = await db.users.col.count_documents({"is_group": False})
-    total_groups = await db.users.col.count_documents({"is_group": True})
+    total_users = db.users.col.count_documents({"is_group": False})
+    total_groups = db.users.col.count_documents({"is_group": True})
 
     await message.reply_text(
         f"Total Users: {total_users}\nTotal Groups: {total_groups}"
